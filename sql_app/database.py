1e937f26b8d4330d4a68112d03595f8095bb4bfe
unknown <kevinlichen1412@gmail.com>
2022-08-09 18:29:57 +0800

    Add random settings for PostgreSQL

--- sql_app/database.py
+++ sql_app/database.py
@@ -13,7 +13,8 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost/ash999"
 
 # With PostgreSQL
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
