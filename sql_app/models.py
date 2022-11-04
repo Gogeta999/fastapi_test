c3fa1787d3433f96e6f9b7391486f89a48af815e
unknown <kevinlichen1412@gmail.com>
2022-11-04 20:18:09 +0800

    In Looping Process

--- sql_app/models.py
+++ sql_app/models.py
@@ -6,11 +6,13 @@ from .database import BaseOld, BaseNew
 class OldMTAccount(BaseOld):
     __tablename__ = "mt_account"
     login = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
     login_type = Column(String(255))
-    commit_group_id = Column(Integer)
-    create_time = Column(DateTime)
-    update_time = Column(DateTime)
+    # commit_group_id = Column(Integer)
+    # create_time = Column(DateTime)
+    # update_time = Column(DateTime)
+    
+    
     # owner_id = Column(Integer, ForeignKey("users.id"))
     # owner = relationship("OldUser", back_populates="accs")
 
@@ -20,35 +22,36 @@ class OldUser(BaseOld):
     user_name = Column(String(255))
     parent_id = Column(Integer)
     role_id = Column(Integer)
-
+    email = Column(String(255))
+   
     
     # accs = relationship("OldMTAccount", back_populates="owner")
 
 class NewMTAccount(BaseNew):
     __tablename__ = "mt_accounts"
     id = Column(String, primary_key=True, index=True)
-    ta_user_id = Column(String(36))
+    ta_user_id = Column(String(36), ForeignKey("ta_base_info.id"))
     mt_account = Column(Integer)
     account_name = Column(String(50))
     account_group = Column(String(30))
     mt_group = Column(String(50))
     mt_acct_type_id = Column(String(36))
-    is_main_account = Column(Integer)
-    mt_server = Column(Integer)
+    is_main_account = Column(Integer)  #Is Auto Created by System or Not
+    mt_server = Column(Integer) # 40 is MT4 demo, 41 is MT4 real, 50 is MT5 Demo, 51 is MT5 Real
     
 class NewTAInfo(BaseNew):
     __tablename__ = "ta_base_info"    
-    id = Column(String(36), primary_key=True, index=True)
+    id = Column(String(36), primary_key=True, index=True) #id from NewMTAcc
     email = Column(String(255))
     name = Column(String(100))
     mt_name = Column(String(255))
-    parent_ib = Column(String(36))
-    parent_type = Column(Integer)
-    ta_type = Column(Integer)
+    parent_ib = Column(String(36), ForeignKey("sys_users.ib"))  #Superior IB or AM
+    parent_type = Column(Integer)  # 1 isAM and 2 is IB 
+    ta_type = Column(Integer)  # 1 is Agent TA, 2 is Pure TA
     
 class NewSYSUser(BaseNew):
     __tablename__ = "sys_users"
-    id = Column(String(36), primary_key=True, index=True)
+    id = Column(String(36), primary_key=True, index=True) #aka parent_ib from TA Info
     login_name = Column(String(255))
     real_name = Column(String(50))
     english_name = Column(String(50))
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -9,12 +9,12 @@ from datetime import datetime
 from pydantic import BaseModel
 
 class OldMTAccount(BaseModel):
-    login: int | None = None
-    user_id: int | None = None
-    login_type: str | None = None
-    commit_group_id: int | None = None
-    create_time: datetime | None = None
-    update_time: datetime | None = None
+    login: int | None
+    user_id: int | None
+    login_type: str | None
+    # commit_group_id: int | None
+    # create_time: datetime | None
+    # update_time: datetime | None
     
     class Config:
         orm_mode = True 
@@ -24,28 +24,28 @@ class OldUser(BaseModel):
     user_name: str
     parent_id: int #User Parent ID
     role_id: int #2 is Agent, 3 is DC
-
-    user_code: str | None
-    password: str | None
     email: str | None
-    role_group_id: int | None
-    channel_id: int | None
-    parent_id: int | None
-    photo: str | None
-    phone: str | None
-    commit_level_id: str | None
-    commit_rule_param: str | None
-    reg_time: datetime | None
-    u_status: int | None
-    info_status: int | None
-    info_review_status: int | None
-    language: str | None
-    invit_code: str | None
-    totp_secret: str | None
-    login_fail_count: int | None
-    last_login_time: datetime | None
-    deposit_group_id: int | None
-    login_verify_code: str | None
+    
+    # user_code: str | None
+    # password: str | None
+    # role_group_id: int | None
+    # channel_id: int | None
+    # parent_id: int | None
+    # photo: str | None
+    # phone: str | None
+    # commit_level_id: str | None
+    # commit_rule_param: str | None
+    # reg_time: datetime | None
+    # u_status: int | None
+    # info_status: int | None
+    # info_review_status: int | None
+    # language: str | None
+    # invit_code: str | None
+    # totp_secret: str | None
+    # login_fail_count: int | None
+    # last_login_time: datetime | None
+    # deposit_group_id: int | None
+    # login_verify_code: str | None
     
     class Config:
         orm_mode = True 
@@ -54,29 +54,29 @@ class NewMTAccount(BaseModel):
     id: str
     ta_user_id: str | int | None
     mt_account: int
-    account_name: str | None = None
+    account_name: str | None
     account_group: str
     mt_group: str
     mt_acct_type_id: str
-    is_main_account: int | None = None #Is Auto Created by System or Not
+    is_main_account: int | None #Is Auto Created by System or Not
     mt_server: int  # 40 is MT4 demo, 41 is MT4 real, 50 is MT5 Demo, 51 is MT5 Real
     
-    leverage: str | None = None
-    open_date: datetime | None = None
-    deal_status: int | None = None
-    enabled_status: int | None = None
-    enabled_date: datetime | None = None
-    enable_change_password: int | None = None
-    enable_otp: int | None = None
-    settlement_type: str | None = None
-    rebate_group_id: str | None = None
-    create_date: datetime | None = None
-    create_userid: str | None = None
-    create_name: str | None = None
-    modify_date: datetime | None = None
-    modify_userid: str | None = None
-    modify_name: str | None = None
-    remarks: str | None = None
+    # leverage: str | None
+    # open_date: datetime | None
+    # deal_status: int | None
+    # enabled_status: int | None
+    # enabled_date: datetime | None
+    # enable_change_password: int | None
+    # enable_otp: int | None
+    # settlement_type: str | None
+    # rebate_group_id: str | None
+    # create_date: datetime | None
+    # create_userid: str | None
+    # create_name: str | None
+    # modify_date: datetime | None
+    # modify_userid: str | None
+    # modify_name: str | None
+    # remarks: str | None
     
     class Config:
         orm_mode = True 
@@ -85,48 +85,51 @@ class NewTAInfo(BaseModel):
     id: str #aka ta_user_id from MT Acc
     email: str
     name: str
-    mt_name: str #Name From MT
-    parent_ib: str #Superior IB or AM
+    mt_name: str | None #Name From MT
+    parent_ib: str | None #Superior IB or AM
     parent_type: int # 1 isAM and 2 is IB
     ta_type: int # 1 is Agent TA, 2 is Pure TA
     
     
-    ident_code: str | None
-    pwd: str | None
-    tel: str | None
-    tel_prefix: str | None
-    tel_is_verify: int | None
-    sex: int | None
-    birthday: str | None
-    nation_id: int | None
-    nation: str | None
-    province: str | None
-    city: str | None
-    county: str | None
-    address: str | None
-    avatar: str | None
-    zip: str | None
-    status: int | None
-    open_acct_status: int | None
-    open_acct_time: datetime | None
-    lock_time: datetime | None
-    mt_servers: str | None
-    payment_group_id: str | None
-    lang: str | None
-    create_date: datetime | None
-    create_userid: str | None
-    create_name: str | None
-    modify_date: datetime | None
-    modify_userid: str | None
-    modify_name: str | None
-    remarks: str | None
-    deleted: int | None
-    delete_date: datetime | None
-    delete_userid: str | None
-    delete_name: str | None
-    superior_code: str | None
-    is_send: int | None
+    # ident_code: str | None
+    # pwd: str | None
+    # tel: str | None
+    # tel_prefix: str | None
+    # tel_is_verify: int | None
+    # sex: int | None
+    # birthday: str | None
+    # nation_id: int | None
+    # nation: str | None
+    # province: str | None
+    # city: str | None
+    # county: str | None
+    # address: str | None
+    # avatar: str | None
+    # zip: str | None
+    # status: int | None
+    # open_acct_status: int | None
+    # open_acct_time: datetime | None
+    # lock_time: datetime | None
+    # mt_servers: str | None
+    # payment_group_id: str | None
+    # lang: str | None
+    # create_date: datetime | None
+    # create_userid: str | None
+    # create_name: str | None
+    # modify_date: datetime | None
+    # modify_userid: str | None
+    # modify_name: str | None
+    # remarks: str | None
+    # deleted: int | None
+    # delete_date: datetime | None
+    # delete_userid: str | None
+    # delete_name: str | None
+    # superior_code: str | None
+    # is_send: int | None
     
+    class Config:
+        orm_mode = True 
+            
 class NewSYSUser(BaseModel):
     id: str #aka parent_ib from TA Info
     login_name:str #most am using Email and login
@@ -139,70 +142,41 @@ class NewSYSUser(BaseModel):
     report_user_id:str | None #superior
     report_user_name:str | None #superior name
     
-    unique_id: str | None
-    login_pwd: str | None
-    avatar: str | None
-    sex: int | None
-    status: int | None
-    subsidiary_depart_ids: str | None
-    subsidiary_depart_names: str | None
-    rebate_level_id: str | None
-    qq: str | None
-    wx: str | None
-    email: str | None
-    office_tel: str | None
-    mobile_phone: str | None
-    login_status: int | None
-    birthday: datetime | None
-    user_number: str | None
-    entry_time: datetime | None
-    lock_time: datetime | None
-    locked_count: int | None
-    create_date: datetime | None
-    create_user: str | None
-    create_userid: str | None
-    modify_date: datetime | None
-    modify_userid: str | None
-    modify_name: str | None
-    remarks: str | None
-    delete_date: datetime | None
-    delete_userid: str | None
-    delete_name: str | None
-    deleted: int | None
-    google_sk: str | None
-    google_auth_band: int | None
-    google_yz_firsttime: datetime | None
-    google_yz_count: int | None
-    google_yz_disable: int | None
-# class ItemBase(BaseModel):
-#     title: str
-#     description: str | None = None
-
-
-# class ItemCreate(ItemBase):
-#     pass
-
-
-# class Item(ItemBase):
-#     id: int
-#     owner_id: int
-
-#     class Config:
-#         orm_mode = True
-
-
-# class UserBase(BaseModel):
-#     email: str
-
-
-# class UserCreate(UserBase):
-#     password: str
-
-
-# class User(UserBase):
-#     id: int
-#     is_active: bool
-#     items: list[Item] = []
-
-#     class Config:
-#         orm_mode = True
+    # unique_id: str | None
+    # login_pwd: str | None
+    # avatar: str | None
+    # sex: int | None
+    # status: int | None
+    # subsidiary_depart_ids: str | None
+    # subsidiary_depart_names: str | None
+    # rebate_level_id: str | None
+    # qq: str | None
+    # wx: str | None
+    # email: str | None
+    # office_tel: str | None
+    # mobile_phone: str | None
+    # login_status: int | None
+    # birthday: datetime | None
+    # user_number: str | None
+    # entry_time: datetime | None
+    # lock_time: datetime | None
+    # locked_count: int | None
+    # create_date: datetime | None
+    # create_user: str | None
+    # create_userid: str | None
+    # modify_date: datetime | None
+    # modify_userid: str | None
+    # modify_name: str | None
+    # remarks: str | None
+    # delete_date: datetime | None
+    # delete_userid: str | None
+    # delete_name: str | None
+    # deleted: int | None
+    # google_sk: str | None
+    # google_auth_band: int | None
+    # google_yz_firsttime: datetime | None
+    # google_yz_count: int | None
+    # google_yz_disable: int | None
+    
+    class Config:
+        orm_mode = True 
