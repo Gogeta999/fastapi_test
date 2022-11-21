be88ef770a067f453aea17d73b25aa71b7b31b3b
unknown <kevinlichen1412@gmail.com>
2022-11-21 21:40:06 +0800

    go

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import Session
 
 from sql_app.models import *
 from sql_app import crud, models, schemas
-from sql_app.database import local_session
+from sql_app.database import local_session, pure_session
+from sqlalchemy import text
 from sql_app import database
 from fastapi.responses import StreamingResponse
 import pandas
@@ -26,7 +27,14 @@ def get_db():
     finally:
         db.close()
 
-
+def pure_sql():
+    db = pure_session()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+        
 @app.get("/")
 def get_home():
     return 'Hello World'
@@ -40,3 +48,13 @@ async def get_4orders(db: Session = Depends(get_db)):
 async def get_5orders(db: Session = Depends(get_db)):
     orders = db.query(MT5OpenOrders).all()
     return orders
+
+@app.get("/test")
+async def test(db: Session = Depends(pure_sql)):
+    array = []
+    sql = text("SELECT * FROM traderecord")
+    results = db.execute(sql)
+    print('yo', type(test))
+    for res in results:
+     array.append(res)
+    return array
